c081717ac947b09f450451f721e41248698c4692
aiya000 <aiya000.develop@gmail.com>
2016-06-04 22:23:39 +0900

    Fix deploy-to-master problem

--- deploy.hs
+++ deploy.hs
@@ -10,7 +10,12 @@ main :: IO ()
 main = shelly . verbosely $ do
   run_ "mv"  ["_site", "/tmp/_site"]
   run_ "git" ["checkout", "master"]
-  files <- ls "/tmp/_site"
-  forM_ files $ \file -> do
+  existFiles <- ls "."
+  forM_ existFiles $ \file -> do
+    if toTextIgnore file == "./.git"
+       then liftIO $ putStrLn ".git directory was found, it didn't deleted"
+       else run_ "rm"  ["-rf", toTextIgnore file]
+  newFiles   <- ls "/tmp/_site"
+  forM_ newFiles $ \file -> do
     run_ "mv"  [toTextIgnore file, "."]
   run_ "rm"  ["-rf", "/tmp/_site"]
