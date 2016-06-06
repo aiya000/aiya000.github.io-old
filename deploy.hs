cf0194b2b95e212838c1c510d7c97f860a30cff1
aiya000 <aiya000.develop@gmail.com>
2016-06-06 18:50:00 +0900

    Update deploy.hs

--- deploy.hs
+++ deploy.hs
@@ -2,20 +2,39 @@
 {-# LANGUAGE ExtendedDefaultRules #-}
 import Control.Monad ( forM_ )
 import Shelly
-import qualified Data.Text as Text
+import Data.Text ( Text, strip, append )
 
-default ( Text.Text )
+default ( Text )
 
 main :: IO ()
-main = shelly . verbosely $ do
-  run_ "mv"  ["_site", "/tmp/_site"]
-  run_ "git" ["checkout", "master"]
+main = shelly . verbosely $
+  deploy `finally_sh` run_ "git" ["checkout", "develop"]
+
+deploy :: Sh ()
+deploy = do
+  run_ "stack" ["exec", "aiya000-github-io", "build"]
+  run_ "cp"    ["-r", "./_site", "/tmp/_site"]
+  run_ "git"   ["checkout", "master"]
+
+  -- Remove all file
   existFiles <- ls "."
   forM_ existFiles $ \file -> do
     if toTextIgnore file == "./.git"
        then liftIO $ putStrLn ".git directory was found, it didn't deleted"
        else run_ "rm"  ["-rf", toTextIgnore file]
+
+  -- Copy backed up files
   newFiles   <- ls "/tmp/_site"
   forM_ newFiles $ \file -> do
     run_ "mv"  [toTextIgnore file, "."]
+
+  -- Clean temporary directory
   run_ "rm"  ["-rf", "/tmp/_site"]
+
+  -- Deploy to repository
+  run_ "git" ["add", "--all"]
+  now <- run "date" ["-R"]
+  let now'    = strip now
+      message = "Build at [" `append` now' `append` "]"
+  run_ "git" ["commit", "-m", message]
+  run_ "git" ["push", "origin", "master"]
