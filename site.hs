f1b176852e4ee590c9485aeb66e0868a758b0b1b
aiya000 <aiya000.develop@gmail.com>
2017-01-23 20:16:30 +0900

    Add "js/*" rule to site.hs

--- site.hs
+++ site.hs
@@ -30,6 +30,10 @@ main = hakyll $ do
     route   idRoute
     compile compressCssCompiler
 
+  match "js/*" $ do
+    route   idRoute
+    compile copyFileCompiler
+
   match "about.md" $ do
     route   $ setExtension "html"
     compile $ pandocCompiler
