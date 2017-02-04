93001ea32b28a84e97a780f6084a06aa520fc7b9
aiya000 <aiya000.develop@gmail.com>
2017-02-04 19:56:53 +0900

    Improve deploy.hs

--- deploy.hs
+++ deploy.hs
@@ -11,37 +11,42 @@ default (Text)
 
 main :: IO ()
 main = shelly . verbosely $ do
-  -- Backup the .stack-work, because .stack-work will be removed when git checked out
+  (onBegin >> deploy) `finally_sh` onEnd
+
+
+onBegin :: Sh ()
+onBegin = do
+  run_ "stack" ["exec", "site", "build"]
   run_ "cp" ["-r", ".stack-work", "/tmp/.stack-work"]
-  deploy `finally_sh` do
-    run_ "git" ["checkout", "develop"]
-    -- Restore the .stack-work
-    run_ "mv" ["/tmp/.stack-work", "."]
+  run_ "cp" ["-r", "_site", "/tmp/_site"]
+  run_ "git" ["checkout", "master"]
+
+onEnd :: Sh ()
+onEnd = do
+  run_ "git" ["checkout", "develop"]
+  run_ "cp" ["-r", "/tmp/.stack-work", ".stack-work"]
+  run_ "rm" ["-rf", "/tmp/_site"]
+
 
 deploy :: Sh ()
 deploy = do
-  run_ "stack" ["exec", "site", "build"]
-  run_ "cp"    ["-r", "./_site", "/tmp/_site"]
-  run_ "git"   ["checkout", "master"]
-
   -- Delete the all file in the master branch
   existFiles <- ls "."
   forM_ existFiles $ \file -> do
+    --TODO: Normalize path of `file`
     when (toTextIgnore file /= "./.git") $
-      run_ "rm"  ["-rf", toTextIgnore file]
+      run_ "rm" ["-rf", toTextIgnore file]
 
   -- Move the backed up contents to here
   newFiles <- ls "/tmp/_site"
   forM_ newFiles $ \file -> do
-    run_ "mv"  [toTextIgnore file, "."]
-
-  -- Delete the temporary directory
-  run_ "rm"  ["-rf", "/tmp/_site"]
-  run_ "rm"  ["-rf", "./_cache"]
+    run_ "mv" [toTextIgnore file, "."]
+  -- _cache is unneeded by the site
+  run_ "rm" ["-rf", "_cache"]
 
   -- Deploy to the repository
   run_ "git" ["add", "--all"]
   now <- strip <$> run "date" ["-R"]
   let message = "Build at [" <> now <> "]"
   run_ "git" ["commit", "-m", message]
-  run_ "git" ["push", "origin", "master"]
+  run_ "git" ["push", "-u", "origin", "master"]
